085d31548116a7b603d7eb1fe48ddb92b5508e5e
Drakefly <csenat15@outlook.com>
2018-02-22 12:36:55 +0100

    Bug detecté pour la gestion des doss

--- src/SerieSimul.java
+++ src/SerieSimul.java
@@ -1,13 +1,16 @@
 import java.util.LinkedList;
 
 public class SerieSimul {
-    public void part3(String doss, int duree){
+    public static void main(String[] args) {
+        String doss = "fichier_pour_test";
+        int duree =100;
         LinkedList <String> fichiers = new LinkedList<>();
         try {
             fichiers = Lecture.lisDoss(doss);
-            for(int i = 0; i < fichiers.size(); i++){
+            for(int i = 0; i < fichiers.size(); i++){//TODO Debug
+                System.out.println("\n \n");
                 System.out.println(fichiers.get(i));
-                new Simulation(duree,fichiers.get(i)).detect();
+                new Simulation(duree,fichiers.get(i)).tourne();
             }
         } catch (FileFormatException e) {
             e.printStackTrace();
