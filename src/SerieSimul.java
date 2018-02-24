fed62932707f2a82fd7847af096908dcd053ef65
Drakefly <csenat15@outlook.com>
2018-02-24 23:00:04 +0100

    HTML MARCHE!!!!

--- src/SerieSimul.java
+++ src/SerieSimul.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class SerieSimul {
@@ -6,10 +8,23 @@ public class SerieSimul {
         LinkedList <String> fichiers ;
         try {
             fichiers = Lecture.lisDoss(doss);
-            String retour="<h1> Les detections de "+doss+ " .</h1>\n\n\n";
+            String retour=
+                    "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "    <head>\n" +
+                    "        <meta charset=\"utf-8\" />\n" +
+                    "        <title>Resultat</title>\n" +
+                    "    </head>\n" +
+                    "\n" +
+                    "    <body>\n" +
+                    "       <h1> Les detections de "+doss+ " .</h1>\n";
             for(int i = 0; i < fichiers.size(); i++) {
                 retour += new Simulation(duree, fichiers.get(i), true).detect(true);
             }
+            retour +=
+                    "    \n" +
+                    "    </body>\n" +
+                    "</html>";
             System.out.println(retour);
             SerieSimul.export(retour,doss);
         } catch (FileFormatException e1) {
@@ -19,15 +34,11 @@ public class SerieSimul {
 
     private static void export (String html,String fichier){//Ill choppe l'html et il le met dans un fichier. On pourrais lui filer un path
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/Users/drakefly/Desktop/DDDDDD/"+fichier+".html")));//Créé un fichier si inexistant
-            FileOutputStream fos;
-            fos = new FileOutputStream(new File(fichier+ ".html"));
-            byte byteRetour[] = html.getBytes();
-            fos.write(byteRetour);
-            fos.close();//TODO verifier ou ca iras.
+            FileWriter writer = new FileWriter(new File("/Users/drakefly/Desktop/DDDDDD/"+fichier+".html"),true);//Créé un fichier si inexistant
+            writer.write(html);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
